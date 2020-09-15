d63e212b78786b15cb3a9b24b3aa1d1a384d075e
fusakoo <obatafusako@gmail.com>
2020-09-15 13:12:03 -0700

    Add documentation

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -1,23 +1,26 @@
 def caesar_cipher(string, shift)
   alphabet = ('a'..'z').to_a
-  alphabet_caps = ('A'..'Z').to_a
+  alphabet_uppercase = ('A'..'Z').to_a
   output = []
 
+  # convert given string into array and loop through each character
   string.chars.each do |character|
     if alphabet.include?(character)
       index = alphabet.index(character)
-
+      
+      # if inputted shift + starting index of character is greater than the length of a-z (26), wrap
       if (index + shift) >= alphabet.length
         shifted_character = (character.ord + shift - 26).chr
         output << shifted_character
+      # otherwise, identify the ascii of character, apply shift, and push shifted character to output array
       else
         shifted_character = (character.ord + shift).chr
         output << shifted_character
       end  
-    elsif alphabet_caps.include?(character)
-      index = alphabet_caps.index(character)
+    elsif alphabet_uppercase.include?(character)
+      index = alphabet_uppercase.index(character)
 
-      if (index + shift) >= alphabet.length
+      if (index + shift) >= alphabet_uppercase.length
         shifted_character = (character.ord + shift - 26).chr
         output << shifted_character
       else  
