b898eb44cdd09602a1bf2103305aae534b6a4366
fusakoo <obatafusako@gmail.com>
2020-09-29 15:15:58 -0700

    update caesar-cipher.rb to partially comply style guide

--- caesar-cipher.rb
+++ caesar-cipher.rb
@@ -7,26 +7,28 @@ def caesar_cipher(string, shift)
   string.chars.each do |character|
     if alphabet.include?(character)
       index = alphabet.index(character)
-      
-      # if inputted shift + starting index of character is greater than the length of a-z (26), wrap
+
+      # if inputted shift + starting index of character is greater
+      # than the length of a-z (26), wrap
       if (index + shift) >= alphabet.length
-        shifted_character = (character.ord + shift - 26).chr
-        output << shifted_character
-      # otherwise, identify the ascii of character, apply shift, and push shifted character to output array
+        (character.ord + shift - 26).chr
+      # otherwise, identify the ascii of character,
+      # apply shift, and push shifted character to output array
       else
         shifted_character = (character.ord + shift).chr
-        output << shifted_character
-      end  
+      end
+      output << shifted_character
+
     elsif alphabet_uppercase.include?(character)
       index = alphabet_uppercase.index(character)
 
       if (index + shift) >= alphabet_uppercase.length
         shifted_character = (character.ord + shift - 26).chr
-        output << shifted_character
-      else  
+      else
         shifted_character = (character.ord + shift).chr
-        output << shifted_character
       end
+      output << shifted_character
+
     else
       output << character
     end
@@ -35,5 +37,5 @@ def caesar_cipher(string, shift)
   p output.join
 end
 
-caesar_cipher("Test!?", 5)
-caesar_cipher("What a string!", 5)
+caesar_cipher('Test!?', 5)
+caesar_cipher('What a string!', 5)
